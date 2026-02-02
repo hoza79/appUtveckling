96bdabe35d26c2a54f5d7af9168eb13154d50e23
Philip <philip.hellzen@gmail.com>
2026-02-02 14:16:58 +0100

    Reverted changes to FirstFragment.java to match main.

--- app/src/main/java/miun/se/laboration1/weatherapp/FirstFragment.java
+++ app/src/main/java/miun/se/laboration1/weatherapp/FirstFragment.java
@@ -7,15 +7,13 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import miun.se.laboration1.weatherapp.databinding.FragmentFirstBinding;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
-
-    private AsyncWeatherWorker<String> worker;
-    private BasicWeatherApi weatherApi;
 
     @Override
     public View onCreateView(
@@ -28,31 +26,7 @@ public class FirstFragment extends Fragment {
 
     }
 
-    /**
-     * Weather Api Call
-     */
-    @Override
-    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        // Create new WeatherApi object
 
-        worker = new AsyncWeatherWorker<>();
-        weatherApi = new BasicWeatherApi();
-
-        worker.start(() -> { return weatherApi.fetchWeather("XML"); },
-
-                new AsyncWeatherWorker.UiCallback<String>() {
-                    @Override
-                    public void onSuccess(String result) {
-                        binding.weatherView.setText(result);
-                    }
-
-                    public void onError(Throwable error) {
-                        String errOut = "Error: " + error.getMessage();
-                        binding.weatherView.setText(errOut);
-                    }
-                }
-        );}
 
     @Override
     public void onDestroyView() {
